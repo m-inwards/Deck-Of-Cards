1f1e34c5c01c5f6dc3d70eab0bad9b80434b24c9
Matt <98755798+m-inwards@users.noreply.github.com>
2022-04-16 13:24:04 +0100

    Began work on game initialization

--- Go_Fish_Game/game.py
+++ Go_Fish_Game/game.py
@@ -1,13 +1,43 @@
 from Card import Card
 from Deck import Deck
 from Hand import Hand
+from datetime import datetime
 
 
 class Game:
-    def __init__(self, players_array):
-        self.players_array = players_array
+    def __init__(self):
+
+        print(f"\033[0;36;1m[{datetime.now().strftime('%H:%M:%S')} INFO]: INITIALIZING GAME\033[0;0m")
+
+        # »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
+        # Generates players array
+        # »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
+        self.players_array = []
+        another_player = True
+
+        while another_player:
+            current_player_name = ""
+            while len(current_player_name) == 0:
+                current_player_name = input("Please enter a player name:\n")
+
+            self.players_array.append((current_player_name, Hand(current_player_name)))
+
+            add_another_player_response = ""
+            while add_another_player_response.lower() not in ["yes", "no", "y", "n"]:
+                add_another_player_response = input("Would you like to add another player? (yes/no)")
+
+            if add_another_player_response in ["no", "n"]:
+                another_player = False
+
+        # »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
+        # Generates standard 52 card deck and shuffles it ready for use
+        # »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
+        self.game_deck = Deck("Go Fish Game Deck")
+        self.game_deck = self.game_deck.generate_standard_deck()
+        self.game_deck = self.game_deck.shuffle_deck()
+
+        print(f"\033[0;36;1m[{datetime.now().strftime('%H:%M:%S')} INFO]: GAME INITIALIZED\033[0;0m")
 
 
 if __name__ == "__main__":
-    test_card = Card("ACE", 1, "SPADES", "BLACK")
-    print(test_card)
+    new_game = Game()
